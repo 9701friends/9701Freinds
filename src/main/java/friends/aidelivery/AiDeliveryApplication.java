3af1d287dd058feec18ecc2823c835eab37c4267
joon <tjrwns3428@gmail.com>
2025-02-13 10:17:00 +0900

    git init

---
+++ src/main/java/friends/aidelivery/AiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package friends.aidelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/friends/aidelivery/AiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package friends.aidelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
