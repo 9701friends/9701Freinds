163672c80515639cc78a0405b97b20ab4eacbbc3
seongmin <seongmin1117@gmail.com>
2025-02-16 21:08:36 +0900

    feat: ProductCategory Entity 생성

---
+++ src/main/java/friends/aidelivery/product/domain/ProductCategory.java
@@ -0,0 +1,60 @@
+package friends.aidelivery.product.domain;
+
+import friends.aidelivery.product.domain.vo.Content;
+import friends.aidelivery.product.domain.vo.Name;
+import friends.aidelivery.store.domain.Store;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_product_category")
+@Entity
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "product_category_id")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Embedded
+    private Name name;
+
+    @Embedded
+    private Content content;
+
+    @OneToMany(mappedBy = "productCategory", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    public ProductCategory(final Store store, final Name name, final Content content) {
+        this.store = store;
+        this.name = name;
+        this.content = content;
+    }
+
+    public ProductCategory(final Store store, final String name, final String content) {
+        this.store = store;
+        this.name = new Name(name);
+        this.content = new Content(content);
+    }
+}
