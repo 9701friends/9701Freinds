734a5b2f757270caa54d3178a196e9f788f0d64d
seongmin <seongmin1117@gmail.com>
2025-02-18 16:25:18 +0900

    feat: 주문 생성시 상품 유효성 검증 구현

--- src/main/java/friends/aidelivery/product/domain/repository/ProductRepository.java
+++ src/main/java/friends/aidelivery/product/domain/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package friends.aidelivery.product.domain.repository;
 
 import friends.aidelivery.product.domain.Product;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ProductRepository {
 
     Product save(Product product);
+
+    Optional<Product> findById(UUID productId);
 }
