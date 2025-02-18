be77b0a0bd8159356e71fb0205c8753a4a2e4e85
hyper <revoluer9@gmail.com>
2025-02-18 11:32:40 +0900

    [feat] 유저 크리에이트 io 반환

---
+++ src/main/java/friends/aidelivery/user/application/dto/request/UserCreateRequest.java
@@ -0,0 +1,15 @@
+package friends.aidelivery.user.application.dto.request;
+
+import friends.aidelivery.user.domain.enums.UserRoleEnum;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserCreateRequest(
+    @NotBlank(message = "이름은 필수입니다.") String name,
+    @NotBlank(message = "이메일은 필수입니다.") String email,
+    @NotBlank String nickname,
+    @NotBlank(message = "비밀번호는 필수입니다.") String password,
+    @NotBlank(message = "권한은 필수입니다.") UserRoleEnum role,
+    @NotBlank(message = "휴대 전화 번호는 필수입니다.") String phone,
+    @NotBlank(message = "주소는 필수입니다.") String address) {
+
+}
