5fa6265285c851a3f77b95c59d9791ca63f010e5
hyper <revoluer9@gmail.com>
2025-02-18 12:05:47 +0900

    [feat] Security 임시 모든 접근 요청 허가

--- src/main/java/friends/aidelivery/common/configuration/SecurityConfig.java
+++ src/main/java/friends/aidelivery/common/configuration/SecurityConfig.java
@@ -2,8 +2,11 @@ package friends.aidelivery.common.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +15,23 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement(sessionManagement ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+            authorizeHttpRequests
+                .requestMatchers("/**").permitAll() //개발 임시 인증 토큰 허용
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
     }
 }
