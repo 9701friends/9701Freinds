46b0769e7fc348c03570de41f23db7d04e98ca9f
hyper <revoluer9@gmail.com>
2025-02-18 11:33:51 +0900

    [feat] 유저 API 등록 컨트롤러 작성

---
+++ src/main/java/friends/aidelivery/user/presentation/UserController.java
@@ -0,0 +1,25 @@
+package friends.aidelivery.user.presentation;
+
+import friends.aidelivery.common.application.dto.CommonResponse;
+import friends.aidelivery.user.application.UserService;
+import friends.aidelivery.user.application.dto.request.UserCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+    @PostMapping("/signIn")
+    public ResponseEntity<CommonResponse> signIn(
+        @RequestBody final UserCreateRequest userCreateRequest) {
+        return userService.singIn(userCreateRequest);
+    }
+}
