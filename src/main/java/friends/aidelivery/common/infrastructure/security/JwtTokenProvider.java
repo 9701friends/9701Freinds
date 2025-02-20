933569d86739396a99ecec8a166a278573abeab2
hyper <revoluer9@gmail.com>
2025-02-20 23:07:48 +0900

    [feat] JWT 토큰에 userID 추가

--- src/main/java/friends/aidelivery/common/infrastructure/security/JwtTokenProvider.java
+++ src/main/java/friends/aidelivery/common/infrastructure/security/JwtTokenProvider.java
@@ -11,8 +11,8 @@ import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -40,12 +40,13 @@ public class JwtTokenProvider {
     }
 
     // 토큰 생성
-    public String createToken(String email, UserRoleEnum role) {
+    public String createToken(String email, UserRoleEnum role, Long userId) {
         Date date = new Date();
         long expirationTime = 86400L * 1000;  // 1일을 밀리초 단위로 변경
 
         return Jwts.builder()
             .setSubject(email)
+            .claim("userId", userId)
             .claim(AUTHORIZATION_KEY, role.getAuthority())
             .setExpiration(new Date(date.getTime() + expirationTime))
             .setIssuedAt(date)
@@ -56,6 +57,7 @@ public class JwtTokenProvider {
     // header 에서 JWT 가져오기
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
         }
@@ -80,16 +82,13 @@ public class JwtTokenProvider {
 
     // JWT Cookie 에 저장
     public void addJwtToCookie(String token, HttpServletResponse res) {
-        try {
-            token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
-            //token = BEARER_PREFIX+token;
-            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token); // Name-Value
-            cookie.setPath("/");
+        token = URLEncoder.encode(BEARER_PREFIX + token, StandardCharsets.UTF_8)
+            .replace("+", "%20");
 
-            // Response 객체에 Cookie 추가
-            res.addCookie(cookie);
-        } catch (UnsupportedEncodingException e) {
-            log.error(e.getMessage());
-        }
+        Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token); // Name-Value
+        cookie.setPath("/");
+
+        // Response 객체에 Cookie 추가
+        res.addCookie(cookie);
     }
 }
