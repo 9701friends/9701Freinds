e4eb364061b969e7934f707561198641bde41296
seongmin <seongmin1117@gmail.com>
2025-02-21 11:19:41 +0900

    feat: 상품카테고리 검색 구현 및 삭제 필드 추가

--- src/main/java/friends/aidelivery/product/application/dto/response/ProductCategoryResponse.java
+++ src/main/java/friends/aidelivery/product/application/dto/response/ProductCategoryResponse.java
@@ -1,19 +1,23 @@
 package friends.aidelivery.product.application.dto.response;
 
 import friends.aidelivery.product.domain.ProductCategory;
+import java.util.List;
 import java.util.UUID;
 
 public record ProductCategoryResponse(
     UUID productCategoryId,
     UUID storeId,
     String name,
-    String content) {
+    String content,
+    List<ProductResponse> products) {
 
     public static ProductCategoryResponse of(final ProductCategory productCategory) {
         return new ProductCategoryResponse(
             productCategory.getId(),
             productCategory.getStore().getId(),
             productCategory.getName().getValue(),
-            productCategory.getContent().getValue());
+            productCategory.getContent().getValue(),
+            productCategory.getProducts().stream().map(ProductResponse::of).toList()
+        );
     }
 }
--- src/main/java/friends/aidelivery/product/infrastructure/repository/ProductCategoryRepositoryImpl.java
+++ src/main/java/friends/aidelivery/product/infrastructure/repository/ProductCategoryRepositoryImpl.java
@@ -1,11 +1,24 @@
 package friends.aidelivery.product.infrastructure.repository;
 
+import static friends.aidelivery.product.domain.QProductCategory.productCategory;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import friends.aidelivery.product.application.dto.request.ProductCategorySearchCond;
 import friends.aidelivery.product.domain.ProductCategory;
 import friends.aidelivery.product.domain.repository.ProductCategoryRepository;
 import friends.aidelivery.product.infrastructure.repository.jpa.ProductCategoryJpaRepository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,6 +26,7 @@ import org.springframework.stereotype.Repository;
 public class ProductCategoryRepositoryImpl implements ProductCategoryRepository {
 
     private final ProductCategoryJpaRepository jpaRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public ProductCategory save(ProductCategory productCategory) {
@@ -22,5 +36,68 @@ public class ProductCategoryRepositoryImpl implements ProductCategoryRepository 
     @Override
     public Optional<ProductCategory> findById(UUID uuid) {
         return jpaRepository.findById(uuid);
+    }
+
+    @Override
+    public Page<ProductCategory> searchCategory(ProductCategorySearchCond cond,
+        Pageable pageable) {
+        BooleanBuilder builder = getBooleanBuilder(cond);
+
+        List<ProductCategory> productCategories = fetchProductCategories(builder,
+            pageable);
+
+        Long total = fetchTotalCount(builder);
+
+        return new PageImpl<>(productCategories, pageable, total);
+    }
+
+    private BooleanBuilder getBooleanBuilder(ProductCategorySearchCond cond) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (cond.storeId() != null) {
+            builder.and(productCategory.store.id.eq(cond.storeId()));
+        }
+
+        if (!cond.isDeleted()) {
+            builder.and(productCategory.isDeleted.eq(false));
+        }
+        return builder;
+    }
+
+    private OrderSpecifier<?>[] getOrderSpecifiers(Pageable pageable) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+        Sort sort = pageable.getSort();
+
+        sort.forEach(order -> {
+            String sortBy = order.getProperty();
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+
+            if ("createdAt".equals(sortBy)) {
+                orderSpecifiers.add(new OrderSpecifier<>(direction, productCategory.createdAt));
+            }
+            if ("updatedAt".equals(sortBy)) {
+                orderSpecifiers.add(new OrderSpecifier<>(direction, productCategory.updatedAt));
+            }
+        });
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
+    }
+
+    private List<ProductCategory> fetchProductCategories(BooleanBuilder builder,
+        Pageable pageable) {
+        return queryFactory.selectFrom(productCategory)
+            .where(builder)
+            .orderBy(getOrderSpecifiers(pageable))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+
+    private Long fetchTotalCount(BooleanBuilder builder) {
+        Long total = queryFactory
+            .select(productCategory.count())
+            .from(productCategory)
+            .where(builder)
+            .fetchOne();
+        return total == null ? 0L : total;
     }
 }
