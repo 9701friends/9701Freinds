5a7d1eac48ecf2d59e01222b0d3ff7b6aca0404f
hyper <revoluer9@gmail.com>
2025-02-21 14:18:00 +0900

    [feat] Validation 검증 커스텀 익셉션 메시지 추가

--- src/main/java/friends/aidelivery/common/presentation/GlobalExceptionHandler.java
+++ src/main/java/friends/aidelivery/common/presentation/GlobalExceptionHandler.java
@@ -6,9 +6,13 @@ import friends.aidelivery.common.exception.CustomForbiddenException;
 import friends.aidelivery.common.exception.CustomNotFoundException;
 import friends.aidelivery.common.exception.CustomUnauthorizedException;
 import friends.aidelivery.common.exception.code.CommonResultCode;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -59,5 +63,30 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(new CommonResponse(CommonResultCode.ERROR.code(),
                 CommonResultCode.ERROR.message()));
+    }
+
+    @ExceptionHandler(value = {AccessDeniedException.class})
+    public ResponseEntity<CommonResponse> handleAccessDeniedException(
+        final AccessDeniedException e) {
+        return ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
+            .body(new CommonResponse(CommonResultCode.FORBIDDEN.code(),
+                CommonResultCode.FORBIDDEN.message()));
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<CommonResponse> handleMethodArgumentNotValidException(
+        final MethodArgumentNotValidException e) {
+
+        List<String> errorMessages = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage) // 오류 메시지만 추출
+            .toList();
+
+        String message = errorMessages.isEmpty() ? "입력값 검증 실패" : errorMessages.get(0);
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new CommonResponse(CommonResultCode.BAD_REQUEST.code(), message));
     }
 }
