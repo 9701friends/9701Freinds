115055373018f2e9ea87e69dd6d7e4de36caa092
seongmin <seongmin1117@gmail.com>
2025-02-22 11:47:22 +0900

    feat: 주문내역 Entity 생성 및 조회 구현

---
+++ src/main/java/friends/aidelivery/order/domain/repository/OrderHistoryRepository.java
@@ -0,0 +1,18 @@
+package friends.aidelivery.order.domain.repository;
+
+import friends.aidelivery.order.domain.OrderHistory;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface OrderHistoryRepository {
+
+    OrderHistory save(OrderHistory orderHistory);
+
+    Optional<OrderHistory> findByOrderId(UUID orderId);
+
+    Page<OrderHistory> search(Long userId, Pageable pageable, String search);
+
+    Page<OrderHistory> findAllByStoreId(UUID storeId, Pageable pageable);
+}
