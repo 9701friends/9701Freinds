646bf96ec3d300fd5770b8227fdc492a5050aa17
seongmin <seongmin1117@gmail.com>
2025-02-22 11:49:51 +0900

    remove: Order에서 OrderProduct 생명주기 관리를 위한 repo 삭제

--- src/main/java/friends/aidelivery/order/infrastructure/jpa/OrderProductJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package friends.aidelivery.order.infrastructure.jpa;
-
-import friends.aidelivery.order.domain.OrderProduct;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductJpaRepository extends JpaRepository<OrderProduct, UUID> {
-
-}
