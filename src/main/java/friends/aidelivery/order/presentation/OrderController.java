7c50864cc5f508b132e1cea1ff838e6414c95f6d
seongmin <seongmin1117@gmail.com>
2025-02-22 11:48:32 +0900

    feat: Order 권한 검증 추가 및 코드 수정

--- src/main/java/friends/aidelivery/order/presentation/OrderController.java
+++ src/main/java/friends/aidelivery/order/presentation/OrderController.java
@@ -7,13 +7,17 @@ import friends.aidelivery.order.application.OrderService;
 import friends.aidelivery.order.application.dto.request.OrderCancelRequest;
 import friends.aidelivery.order.application.dto.request.OrderCreateRequest;
 import friends.aidelivery.order.application.dto.response.OrderResponse;
+import friends.aidelivery.order.domain.enums.OrderStatus;
+import friends.aidelivery.user.domain.enums.UserRoleEnum.Authority;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,33 +43,37 @@ public class OrderController {
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @Valid @RequestBody OrderCreateRequest request) {
         OrderResponse response = orderService.createOrder(request,
-            userDetails.getEmail());
+            userDetails);
         return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(response),
             HttpStatus.CREATED);
     }
 
+    @Secured({Authority.CUSTOMER, Authority.MANAGER, Authority.MASTER})
     @PostMapping("{orderId}/cancel")
     public ResponseEntity<CommonResponse> cancelOrder(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable UUID orderId,
         @RequestBody OrderCancelRequest request) {
-        orderService.cancelOrder(orderId, userDetails.getEmail(), request);
+        orderService.cancelOrder(orderId, userDetails, request);
         return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(), HttpStatus.OK);
     }
 
-    @PostMapping("{orderId}/accept")
-    public ResponseEntity<CommonResponse> acceptOrder(
+    @Secured({Authority.OWNER, Authority.MANAGER, Authority.MASTER})
+    @PatchMapping("{orderId}/status")
+    public ResponseEntity<CommonResponse> updateOrderStatus(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @PathVariable UUID orderId) {
-        orderService.acceptOrder(orderId, userDetails.getEmail());
+        @PathVariable UUID orderId,
+        @RequestBody OrderStatus status) {
+        orderService.updateOrderStatus(orderId, userDetails, status);
         return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(), HttpStatus.OK);
     }
 
-    @PostMapping("{orderId}/reject")
-    public ResponseEntity<CommonResponse> rejectOrder(
+    @Secured({Authority.OWNER, Authority.MANAGER, Authority.MASTER})
+    @PostMapping("{orderId}/complete")
+    public ResponseEntity<CommonResponse> completeOrder(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable UUID orderId) {
-        orderService.rejectOrder(orderId, userDetails.getEmail());
+        orderService.completeOrder(orderId, userDetails);
         return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(), HttpStatus.OK);
     }
 }
